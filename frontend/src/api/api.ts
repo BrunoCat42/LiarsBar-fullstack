ab1fb08f67c67ae093c0b01d0f9605512e29c12d
Bruno <bruno.lorenzi.catunda@gmail.com>
2026-04-21 20:08:26 +0200

    correct url calling

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,19 +1,9 @@
 import axios from 'axios';
 
-let baseURL: string;
-
-if (import.meta.env.DEV) {
-  baseURL = 'http://localhost:3001/api';
-} else {
-  baseURL = '/api';
-}
+const baseURL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  // Sets the base URL for all API requests to our backend server.
-
-  baseURL: baseURL,
-
-  //This instructs Axios to include credentials (like cookies) in all cross-origin requests, allowing the backend to read the auth token
+  baseURL: `${baseURL}/api`,
   withCredentials: true,
 });
 
